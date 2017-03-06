005cd8f94b7f942cb97a82863770dcecded51d90
Scott Ouellette <scottx611x@gmail.com>
2017-03-06 12:26:05 -0500

    Update app reference

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-"""monkey URL Configuration
+"""resource_manager URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
