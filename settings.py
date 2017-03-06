f43d177f0947a041876c834c6b080118b556bfc1
JakeConway <jakeconway45@gmail.com>
2017-03-06 12:46:41 -0500

    add AWS database configurations

--- settings.py
+++ settings.py
@@ -69,17 +69,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+# For information on how to add multiple databases, and give them an * ALIAS *, check out:
+# https://docs.djangoproject.com/en/dev/topics/db/multi-db/#automatic-database-routing
+# AWS RD information: http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create-deploy-python-rds.html
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
