170ccdecaa9d66f79270b805b8c6ffa8c3ff5a9e
Scott Ouellette <scottx611x@gmail.com>
2017-03-07 12:35:57 -0500

    lots of hacks

--- resource_manager/views.py
+++ resource_manager/views.py
@@ -1,14 +1,14 @@
-import json
-
+import boto3
+from django.conf import settings
 from django.db import transaction, IntegrityError
-from django.forms import model_to_dict
-from django.http import HttpResponseServerError, JsonResponse
+from django.http import HttpResponseServerError
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from resource_manager.models import JupyterUser, JupyterNode
+from resource_manager.models import JupyterUser
 from resource_manager.serializers import JupyterUserSerializer
-from resource_manager.utils import get_fullest_node, create_ebs_volume
+from resource_manager.utils import get_fullest_node, create_ebs_volume, \
+    attach_ebs_volume, mount_linux_device
 
 
 class JupyterUserViewSet(viewsets.ModelViewSet):
@@ -20,6 +20,7 @@ class JupyterUserViewSet(viewsets.ModelViewSet):
     lookup_value_regex = '[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}'
     lookup_field = "email"
     http_method_names = ["get", "put"]
+    ec2_client = boto3.client('ec2')
 
     def get_object(self):
         if self.request.method == 'PUT':
@@ -46,6 +47,12 @@ class JupyterUserViewSet(viewsets.ModelViewSet):
                     jupyter_user.node = node
                     jupyter_user.save()
 
+                    sys_devices = {
+                        "50001": "/dev/sdh",
+                        "50002": "/dev/sdi",
+                        "50003": "/dev/sdj",
+                        "50004": "/dev/sdk"
+                    }
                     for port in ["50001", "50002", "50003", "50004"]:
                         try:
                             jupyter_user.port = port
@@ -54,9 +61,34 @@ class JupyterUserViewSet(viewsets.ModelViewSet):
                         except IntegrityError:
                             continue
                         else:
+                            special_device = sys_devices[port]
                             ebs_id = create_ebs_volume()
                             jupyter_user.ebs_volume_id = ebs_id
                             jupyter_user.save()
+
+                            is_ebs_attached = attach_ebs_volume(
+                                jupyter_user.ebs_volume_id,
+                                jupyter_user.node.ec2_id,
+                                special_device
+                            )
+
+                            jupyter_user.ebs_volume_attached = is_ebs_attached
+                            jupyter_user.save()
+                            ec2_info = self.ec2_client.describe_instances(
+                                DryRun=False,
+                                InstanceIds=[jupyter_user.node.ec2_id],
+                            )
+                            public_dns = ec2_info["Reservations"][0]["Instances"][0]["PublicDnsName"]
+
+                            try:
+                                mount_linux_device(
+                                    public_dns,
+                                    settings.OS_USERNAME,
+                                    special_device,
+                                    jupyter_user.volume_name
+                                )
+                            except Exception as e:
+                                print e
 
                             return jupyter_user
                 else:
